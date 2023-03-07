86aa287fa23dc529caf0003e05d90c148523476c
Andrej <andrej@artistika.com>
2023-03-07 16:48:00 +0100

    removing slick css for now

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { PlasmicRootProvider } from '@plasmicapp/react-web';
 import Helmet from 'react-helmet';
-import './node_modules/slick-carousel/slick/slick.css';
-import './node_modules/slick-carousel/slick/slick-theme.css';
-import '@plasmicpkgs/antd/dist/antd.css';
+// import './node_modules/slick-carousel/slick/slick.css';
+// import './node_modules/slick-carousel/slick/slick-theme.css';
+// import '@plasmicpkgs/antd/dist/antd.css';
+
 export const wrapRootElement = ({ element }) => {
   return <PlasmicRootProvider Head={Helmet}>{element}</PlasmicRootProvider>;
 };
