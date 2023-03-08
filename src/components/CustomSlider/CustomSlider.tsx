a579e610226d58247334c43719ae08ac2b7a9984
Andrej <andrej@artistika.com>
2023-03-08 15:41:04 +0100

    added hack for customslider

--- src/components/CustomSlider/CustomSlider.tsx
+++ src/components/CustomSlider/CustomSlider.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Slider from 'react-slick';
-import './CustomSlider.css';
+// import './CustomSlider.css';
 export default function CustomSlider({ children, ...props }) {
   const [dragging, setDragging] = useState(false);
 
@@ -11,6 +11,13 @@ export default function CustomSlider({ children, ...props }) {
         e.stopPropagation();
         e.preventDefault();
         setDragging(false);
+        var anchorTags = document.getElementsByTagName('a');
+
+        var anchorsList = Array.prototype.slice.call(anchorTags);
+
+        anchorsList.forEach((el) => {
+          el.style.pointerEvents = 'auto';
+        });
       }
     },
     [dragging]
@@ -20,9 +27,32 @@ export default function CustomSlider({ children, ...props }) {
     setDragging(true);
   }, [setDragging]);
 
-  console.log('dragging', dragging);
+  console.log('is dragging', dragging);
+
+  useEffect(() => {
+    // console.log(document.getElementsByTagName('a'));
+    var anchorTags = document.getElementsByTagName('a');
+
+    var anchorsList = Array.prototype.slice.call(anchorTags);
+
+    anchorsList.forEach((el) => {
+      el.style.pointerEvents = 'none';
+    });
+
+    // for (let elem in anchorsList) {
+    //   console.log(elem);
+    // }
+  }, [dragging]);
+
   return (
-    <Slider onSwipe={handleSwiped} {...props}>
+    <Slider
+      swipeEvent={(event) => {
+        console.log(event);
+        setDragging(true);
+      }}
+      onSwipe={handleSwiped}
+      {...props}
+    >
       {React.Children.map(children, (child) => (
         <div onClickCapture={handleOnItemClick}>{child}</div>
       ))}
